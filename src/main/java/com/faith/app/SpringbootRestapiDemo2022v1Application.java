7413ce83acb61eb3674e90c4c5fed4b26633b1ef
sahadan <sahadan@gmail.com>
2021-08-17 22:29:22 +0530

    Initial commit

---
+++ src/main/java/com/faith/app/SpringbootRestapiDemo2022v1Application.java
@@ -0,0 +1,13 @@
+package com.faith.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestapiDemo2022v1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestapiDemo2022v1Application.class, args);
+	}
+
+}
